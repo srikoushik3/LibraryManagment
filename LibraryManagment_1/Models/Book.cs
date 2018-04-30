0a91fc301ee928cb49b5c28552324094bbd7a131
Sri Koushik Nellutla <sk2999ne@edu.uwaterloo.ca>
2018-04-30 11:29:30 -0400

    Initial Commit

---
+++ LibraryManagment_1/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryManagment_1.Models
+{
+    public class Book
+    {
+        public string title;
+        public int isbn;
+        public string author;
+        public string description;
+        public int idNum;
+    }
+}
---
+++ LibraryManagment_1/Models/IdentityModels.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+/*
+using Microsoft.Data.Entity;
+using Microsoft.Data.Entity.Metadata;
+using Microsoft.Framework.OptionsModel;
+*/
+namespace LibraryManagment_1.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public ApplicationDbContext()
+            : base(Database1, false)
+        {
+        }
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
